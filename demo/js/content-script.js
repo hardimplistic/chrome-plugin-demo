d02e7db3ee586ea2a246c9d963b1e069a7d9ca04
LN.impler <lnimpler@163.com>
2018-08-23 17:29:54 +0800

    1

--- demo/js/content-script.js
+++ demo/js/content-script.js
@@ -1,4 +1,5 @@
 ﻿console.log('这是content script!');
+console.log('这是content script! hei hei hei!');
 
 // 注意，必须设置了run_at=document_start 此段代码才会生效
 document.addEventListener('DOMContentLoaded', function()
